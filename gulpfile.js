035710680154a12198478e407a9e8b7a97f6377a
Akuukis <akuukis@kalvis.lv>
2016-12-17 20:49:40 +0200

    Add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,32 @@
-var gulp = require('gulp');
-var screeps = require('gulp-screeps');
-var credentials = require('./credentials.js');
+const gulp = require('gulp');
+const gulpScreeps = require('gulp-screeps');
+const eslint = require('gulp-eslint');
 
-gulp.task('screeps', function() {
-  gulp.src('*.js')
-    .pipe(screeps(credentials));
+const credentials = require('./credentials.js');
+const SRC = 'src/**/*.js';
+const BUILD = 'build/';
+const LIB = 'lib/';
+
+gulp.task('lint', _=>
+    gulp.src(SRC)
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+);
+
+gulp.task('build', function() {
+	gulp.src(SRC)
+		.pipe(gulp.dest(BUILD))
 });
 
 gulp.task('test', function() {
 	console.log('TODO: write tests in Mocha.')
 });
 
-gulp.task('default', ['screeps']);
+gulp.task('commit', _=>
+    gulp.src(BUILD)
+        .pipe(gulpScreeps(credentials))
+);
+
+gulp.task('default', ['lint', 'build', 'test', 'commit']);
+gulp.task('watch', _=> gulp.watch(SRC, ['default']));
