d6fbf21b3a6fe7046679997f417f4b106197c0d4
Akuukis <akuukis@kalvis.lv>
2016-12-18 23:04:23 +0200

    Cleanup main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+//// Add new functions.
 // var _ = require('lodash');
 global.logger = require('./logger');
 global.utils = require('./utils');
@@ -8,37 +10,37 @@ global.getID = global.utils.getID;
 global.gobi = global.utils.gobi;
 global.posify = global.utils.posify;
 
-{  //// Extend existing classes.
-  let extend = function extend(screepsClass, newMethods){
-    for(let key in newMethods) screepsClass.prototype[key] = newMethods[key];
-  };
-  extend(global.Room, require('extendRoom'));
-}
 
-{ //// New classes.
-  global.Agent = require('./economy').Agent;
-}
+//// Extend existing classes.
+let extend = function extend(screepsClass, newMethods){
+  for(let key in newMethods) screepsClass.prototype[key] = newMethods[key];
+};
+extend(global.Room, require('extendRoom'));
 
-{ //// Global entities: Build from Memory to RAM.
-  global.utils.checkMemory();
-  let Defer = require('./defer');
-  Game.player = require('./player');
-  Game.bazaar = require('./economy').bazaar;
-  Game.defer = {
-    high: new Defer('high'),
-    medium: new Defer('medium'),
-    low: new Defer('low'),
-  };
-}
+
+//// Add new classes.
+global.Agent = require('./economy').Agent;
+
+
+//// Add new entities.
+global.utils.checkMemory();
+Game.player = require('./player');
+Game.bazaar = require('./economy').bazaar;
+let Defer = require('./defer');
+Game.defer = {
+  high: new Defer('high', 50),
+  medium: new Defer('medium', 20),
+  low: new Defer('low', 10),
+};
+
 
 global.logger.info('Reinitiated.');
 
-module.exports.loop = function() {
+
+module.exports.loop = function(){
   // return;
   Game.player.loop();  //  priority
   Game.defer.high.loop();  // High priority
   Game.defer.medium.loop();  // Medium priority
   Game.defer.low.loop();  // Low priority
 };
-
-// END. Leave empty line below.
